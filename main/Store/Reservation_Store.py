af3f43682b52edac53ae728513cb150fc717ca3b
100479095 <100479095@alumnos.uc3m.es>
2024-09-29 18:09:01 +0200

    Load y save store de las distintas clases

--- main/Store/Reservation_Store.py
+++ main/Store/Reservation_Store.py
@@ -6,3 +6,23 @@ class ReservationStore(JsonStore):
 
     def __init__(self):
         super().__init__(STORE_NAME)
+
+    def load_store(self):
+        """ Leo los datos del fichero si existe , y si no existe creo una lista vacia"""
+        try:
+            with open(self._file_name, "r", encoding="utf-8", newline="") as file:
+                self._data_list = json.load(file)
+        except FileNotFoundError:
+            self._data_list = []
+        except json.JSONDecodeError:
+            raise FileNotFoundError("The file doesn't exist")
+
+        return self._data_list
+
+    def save_store(self):
+        """Subimos al almacen los datos"""
+        try:
+            with open(self._file_name, "w", encoding="utf-8", newline="") as file:
+                json.dump(self.data_list, file, indent=2)
+        except FileNotFoundError:
+            raise FileNotFoundError("The file doesn't exist")
