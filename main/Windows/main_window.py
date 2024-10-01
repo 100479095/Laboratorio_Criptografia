bdf607fb102d844e4ec6da79eee97b8368942bec
100479095 <100479095@alumnos.uc3m.es>
2024-10-01 15:45:05 +0200

    Modelado de la ventana principal

--- main/Windows/main_window.py
+++ main/Windows/main_window.py
@@ -1,15 +1,40 @@
 import tkinter as tk
+from tkinter import ttk
 
 class Log_Window (tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Log_Window")
         self.geometry("700x500")
-        self.minsize(700, 500)
-        self.items_creation()
+        self.resizable(False, False)
+        self.main_frame()
 
         self.mainloop()
 
-    def items_creation (self):
-        main_title = tk.Label(self, text = "Canchas de Padel", font = ("Verdana", 24), bg="green", fg="white", width="700")
-        main_title.pack(anchor = tk.CENTER)
+    def main_frame (self):
+        main_frame = tk.Frame(self,width=700, height=500, bg="lightblue")
+        main_frame.pack()
+        main_title = tk.Label(main_frame, text = "Canchas de Padel", font = ("Verdana", 24), bg="darkblue", fg="white") #width="700")
+        main_title.grid(column= 0, row=0, columnspan=5, ipadx= 225, ipady=30, padx=0, pady=0)
+
+        #Títulos
+        name_label = tk.Label(main_frame, text="Nombre: ", font = ("Verdana"))
+        name_label.grid(column=1, row=1, ipadx=25, ipady=5, padx=50, pady=30)
+        password_label = tk.Label(main_frame, text="Contraseña: ", font = ("Verdana"))
+        password_label.grid(column=1, row=2, ipadx=5, ipady=5, padx=50, pady=30)
+
+        #Entradas
+        user_name = tk.StringVar()
+        name_entry = tk.Entry(main_frame, textvariable= user_name, font = ("Verdana"))
+        name_entry.grid(column=3, row=1, ipadx=5, ipady=5, padx=40, pady=20)
+
+        user_pass = tk.StringVar()
+        password_entry = tk.Entry(main_frame, textvariable= user_pass, font = ("Verdana"), show='*')
+        password_entry.grid(column=3, row=2, ipadx=5, ipady=5, padx=40, pady=20)
+
+        #botones
+        login_button = ttk.Button(main_frame, text="Log-In")#, font = ("Verdana"))
+        login_button.grid(column=3, row=5, ipadx=5, ipady=5, padx=10, pady=10)
+
+        register_button = ttk.Button(main_frame, text="Registrate")#, font = ("Verdana"))
+        register_button.grid(column=1, row=5, ipadx=5, ipady=5, padx=10, pady=10)
