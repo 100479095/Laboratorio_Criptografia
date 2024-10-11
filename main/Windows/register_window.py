4060a94987c9af7acd7102b05e38949efc9276b0
100479095 <100479095@alumnos.uc3m.es>
2024-10-11 20:12:11 +0200

    cambios ventana de registro

--- main/Windows/register_window.py
+++ main/Windows/register_window.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 
 class Register_Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Register_Window")
-        self.geometry("700x500")
+        self.geometry("700x600")
         self.resizable(False, False)
         self.create_register_window()
 
@@ -21,8 +22,10 @@ class Register_Window(tk.Tk):
         username_label.grid(column=1, row=1, ipadx=7, ipady=5, padx=50, pady=30)
         password_label = tk.Label(main_frame, text="Contraseña: ", font=("Verdana"))
         password_label.grid(column=1, row=2, ipadx=45, ipady=5, padx=50, pady=30)
-        password_label2 = tk.Label(main_frame, text="Repetir contraseña: ", font=("Verdana"))
-        password_label2.grid(column=1, row=3, ipadx=9, ipady=5, padx=50, pady=30)
+        fullname_label = tk.Label(main_frame, text="Nombre y Apellido: ", font=("Verdana"))
+        fullname_label.grid(column=1, row=3, ipadx=9, ipady=5, padx=50, pady=30)
+        creditcard_label = tk.Label(main_frame, text="Tarjeta de Crédito: ", font=("Verdana"))
+        creditcard_label.grid(column=1, row=4, ipadx=9, ipady=5, padx=50, pady=30)
 
         # Entradas
         user_name = tk.StringVar()
@@ -33,7 +36,13 @@ class Register_Window(tk.Tk):
         password_entry = tk.Entry(main_frame, textvariable=user_pass, font=("Verdana"), show='*')
         password_entry.grid(column=3, row=2, ipadx=5, ipady=5, padx=40, pady=20)
 
-        user_pass2 = tk.StringVar()
-        password_entry2 = tk.Entry(main_frame, textvariable=user_pass2, font=("Verdana"), show='*')
-        password_entry2.grid(column=3, row=3, ipadx=5, ipady=5, padx=40, pady=20)
+        user_full_name = tk.StringVar()
+        full_name_entry = tk.Entry(main_frame, textvariable=user_full_name, font=("Verdana"))
+        full_name_entry.grid(column=3, row=3, ipadx=5, ipady=5, padx=40, pady=20)
 
+        user_creditcard = tk.StringVar()
+        creditcard_entry= tk.Entry(main_frame, textvariable=user_creditcard, font=("Verdana"))
+        creditcard_entry.grid(column=3, row=4, ipadx=5, ipady=5, padx=40, pady=20)
+
+        register_button = ttk.Button(main_frame, text="Registrate")#, command=self.log_in)  # , font = ("Verdana"))
+        register_button.grid(column=2, row=5, ipadx=5, ipady=5, padx=10, pady=10)
