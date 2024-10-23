4a536f3f3d3096898eb86ba07786d8074b8e0bc2
Joaquín Pujol Carrillo <100495826@gmail.com>
2024-10-23 12:49:51 +0200

    función autenthicate

--- main/Store/User_Store.py
+++ main/Store/User_Store.py
@@ -1,9 +1,11 @@
+import base64
 import json
 from Store.json_store import JsonStore
 from Classes.User import User
 import struct, os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from Keys.Key import ChaChaKey
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
 STORE_NAME= "Users.json"
 class UserStore(JsonStore):
@@ -24,10 +26,19 @@ class UserStore(JsonStore):
         """"Verificamos si el usuario existe en la base de datos y lo autenticamos con su contraseña"""
         user = self.find_user(username)
         if user is not None:
-           if user["password"] == password:
-               return (User(user["username"], user["password"], user["name"], user["creditcard"]))
-        else:
-            return None
+            kdf= Scrypt(
+                salt = user["password_salt"],
+                length =32,
+                n= 2**14,
+                r=8,
+                p=1
+            )
+            token1 = user["password_token"]
+            kdf.verify(base64.b64decode(password.encode()), token1)
+           #if user["password"] == password:
+               #return (User(user["username"], user["password"], user["name"], user["creditcard"]))
+        #else:
+            #return None
 
     def register_user(self, user=dict):
         self.add_store(user)
