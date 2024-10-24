796a9c4d6401a8727b9f75d935bc8bbcba885905
100479095 <100479095@alumnos.uc3m.es>
2024-10-24 21:10:28 +0200

    Página de reserva de canchas

--- main/Windows/reservar_window.py
+++ main/Windows/reservar_window.py
@@ -1,10 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 
 class Reservar_Window(tk.Tk):
     def __init__(self, user):
         super().__init__()
         self.title("Register_Window")
-        self.geometry("700x500")
+        self.geometry("700x400")
         self.resizable(False, False)
         self.user = user
         self.create_reservar_window()
@@ -17,5 +18,21 @@ class Reservar_Window(tk.Tk):
         main_title.grid(column=0, row=0, columnspan=5, ipadx=325, ipady=30, padx=0, pady=0)
 
         # Títulos
-        bienvenida_label = tk.Label(main_frame, text="Bievenido!", font=("Verdana", 32), bg="lightblue", fg="white")
-        bienvenida_label.grid(column=2, row=1, ipadx=7, ipady=5, padx=50, pady=0)
+        name_user = self.user.name.decode("utf-8")
+        bienvenida_label = tk.Label(main_frame, text=f"Bievenido {name_user}!", font=("Verdana", 32), bg="lightblue", fg="black")
+        bienvenida_label.grid(column=0, row=1, columnspan=5,  ipadx=7, ipady=5, padx=50, pady=0)
+
+        bienvenida_label = tk.Label(main_frame, text=f"Elige la cancha que quieras reservar", font=("Verdana", 24), bg="lightblue",fg="black")
+        bienvenida_label.grid(column=0, row=2, columnspan=5,  ipadx=7, ipady=5, padx=50, pady=0)
+
+        cancha_1 = tk.Button(main_frame, text="Cancha 1", font=("Verdana", 20), bg="darkblue", fg="white")
+        cancha_1.grid(column=1, row=3, ipadx=5, ipady=7, padx=10, pady=10)
+
+        cancha_2 = tk.Button(main_frame, text="Cancha 2", font=("Verdana", 20), bg="darkblue", fg="white")
+        cancha_2.grid(column=2, row=3, ipadx=5, ipady=7, padx=10, pady=10)
+
+        cancha_3 = tk.Button(main_frame, text="Cancha 3", font=("Verdana", 20), bg="darkblue", fg="white")
+        cancha_3.grid(column=1, row=4, ipadx=5, ipady=5, padx=10, pady=10)
+
+        cancha_4 = tk.Button(main_frame, text="Cancha 4", font=("Verdana", 20), bg="darkblue", fg="white")
+        cancha_4.grid(column=2, row=4, ipadx=5, ipady=5, padx=10, pady=10)
